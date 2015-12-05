ba765050ecc1aa6a308937623640a5c2ba05b1de
Ksenia Mikhailovskaya <kseniamikhailovskaya@Ksenias-MacBook-Pro.local>
2015-12-04 19:50:19 -0500

    Final Submission

--- GuessingGameJS.js
+++ GuessingGameJS.js
@@ -12,31 +12,28 @@ function generateWinningNumber(num){
 // Fetch the Players Guess
 var $numGuesses = 7;
 
+var guessArr = [];
 
 function playersGuessSubmission(){
 // Add functionality to the playersGuessSubmission function to grab data (the Player's guess) from the input field when the Submit Guess button is clicked and assign the data to the playersGuess variable.
-  playersGuess = +$("#UserInput").val();
-  //guessArr.push(playersGuess);
-
- //push the guesses to the array
-  checkForRepeats(guessArr, playersGuess);
-  checkGuess();
-
+playersGuess = +$("#UserInput").val();
+checkGuess();
+guessArr.push(playersGuess);
 // Remove the data (Player's guess) from the input field from the DOM since it is already stored in our Web Browser's memory.
 // Write User Guess in h4
-  $("#UserInput").val("");
+$("#UserInput").val("");
 }
 
 // Determine if the next guess should be a lower or higher number
 //var $m2 = lowerOrHigher();
 function lowerOrHigher(){
-    var $mes;
-      if(playersGuess < winningNumber) {
-        $mes = "Try higher!";
-    }
-      else if(playersGuess > winningNumber) {
-        $mes = "Try lower!";
-    }
+  var $mes;
+  if(playersGuess < winningNumber) {
+    $mes = "Try higher!";
+  }
+  else if(playersGuess > winningNumber) {
+    $mes = "Try lower!";
+  }
   return $mes;
 }
 
@@ -61,95 +58,80 @@ function guessMessage() {
 
 // Check if the Player's Guess is the winning number 
 function preventDefault(x) {
-
   x.click(function( event ) {
-  event.preventDefault();
-  $( "<div>" )
+    event.preventDefault();
+    $( "<div>" )
     .append( 'Click "Play Again" to try one more time' )
-    .appendTo( $("#Message").empty() );
-    $("#NumberMessage").empty();
+    .appendTo( $("#NumberMessage").empty() );
+    $("#Message").empty();
     $("#RemainingGuesses").empty();
-});
+  });
 }
 
 //??? DOESN"T WORK. Where is a bug?
 
-var guessArr = [];
+
 
 function checkForRepeats(arr,currentGuess) {
-    //guessArr.push(playersGuess);
+  var repeats;
+  var repeats = true;
 
-    if(arr.indexOf(currentGuess) === 0) {
-      $("#NumberMessage").text("Your number is " + playersGuess);
-      $("#Message").text("You have already tried this number. Try another one!");
-    
-    } else {
-      guessArr.push(currentGuess);
-      //checkGuess();
-        // $("#NumberMessage").text("Your number is " + playersGuess);
-        // $("#Message").text("You have already tried this number. Try another one!");
+  if(arr.indexOf(currentGuess) === -1) {
+      //guessArr.push(currentGuess);
+      repeats = false;
     }
+    return repeats;
+  }
 
-    alert(guessArr);
-}
+  function checkGuess(){
 
-function checkGuess(){
-
-  if(playersGuess === winningNumber) {
+    if(playersGuess === winningNumber) {
+     preventDefault($("#Submit"));
+     preventDefault($("#Hint"));
      $("#NumberMessage").text("Your number is " + playersGuess);
      $("#Message").text("YOU WON!!!");
-     $("#RemainingGuesses").text('Click "Play Again" to try one more time');
-
      $("body").css("background-image", "url('http://cliparts.co/cliparts/pi7/8gq/pi78gqb7T.jpg')");
-     $("h1, h2, #YouWon").css("display", "none");
-     $(".paddingTop230").css("padding-top", "350px");
+     $("h1, h2, #InputField, #RemainingGuesses, #Hint").hide();
+     $(".paddingTop230").css("padding-top", "390px");
      $("#Message").css("font-size", "70px");
-
-     //or if elements are not hidden:
-     // preventDefault($("#Submit"));
-     // preventDefault($("#Hint"));
 
    }
 
-  else {
+   else {
+    if(isNaN(playersGuess) || playersGuess === 0) {
+      $("#NumberMessage").text("This is not a number");
+      $("#Message").text("You should enter a valid number!");
+    } 
 
-      if(isNaN(playersGuess) || playersGuess === 0) {
-        $("#NumberMessage").text("This is not a number");
-        $("#Message").text("You should enter a valid number!");
-      } 
+    else if(playersGuess < 0 || playersGuess > 100) {
+      $("#NumberMessage").text("Your number is " + playersGuess);
+      $("#Message").text("This number is not in the range from 1 to 100. Try another one!");
 
-      else if(playersGuess < 0 || playersGuess > 100) {
-        $("#NumberMessage").text("Your number is " + playersGuess);
-        $("#Message").text("This number is not in the range from 1 to 100. Try another one!");
+    }
 
-      }
+    else if(checkForRepeats(guessArr, playersGuess) === true) {
+      $("#NumberMessage").text("Your number is " + playersGuess);
+      $("#Message").text("You have already tried this number. Try another one!");
+    }
 
-      //check for the repeated User Inputs
-      //??? DOESN"T WORK. Where is a bug?
-      // else if(!checkForRepeatedEntries(guessArr)) {
-      //   $("#NumberMessage").text("Your number is " + playersGuess);
-      //   $("#Message").text("You have already tried this number. Try another one!");
-      // }
-
-      else {
-        $numGuesses--;
-        $("#NumberMessage").text("Your number is " + playersGuess);
-        $("#RemainingGuesses").text($numGuesses + " attempts left!");
-        guessMessage();
-      }
+    else {
+      $numGuesses--;
+      $("#NumberMessage").text("Your number is " + playersGuess);
+      $("#RemainingGuesses").text($numGuesses + " attempts left!");
+      guessMessage();
+    }
   }
 
   if($numGuesses < 1) {
-     $("#Message").text("You ran out of attempts. LOOSER!")
-     $("#RemainingGuesses").text('Click "Play Again" to try one more time');
-     preventDefault($("#Submit"));
-     preventDefault($("#Hint"));
-  }
+   $("#Message").text("You ran out of attempts. LOOSER!")
+   $("#RemainingGuesses").text('Click "Play Again" to try one more time');
+   preventDefault($("#Submit"));
+   $("#Hint, #InputField").hide();
+ }
 }
 
 // Create a provide hint button that provides additional clues to the "Player"
 function provideHint(){
-
   var $h0 = "Your number is one of the three numbers: "; 
   var $h1 = generateWinningNumber(100);
   var $h2 = generateWinningNumber(100);
@@ -160,15 +142,27 @@ function provideHint(){
     $h2 = generateWinningNumber(100);
   }
   
-  var randArr = [];
-  //[W||L, W||L, W||L]
-  randArr[0] = $h1;
-  randArr[1] = $h2;
-  randArr[2] = $h3;
-  randArr[generateWinningNumber(2)] = $h3;
+  var randArr = [$h1, $h2, $h3];
+  shuffle(randArr);
 
- //return randArr; 
   $("#Message").text($h0 + "  "+ randArr[0] + "  " + randArr[1] + "  " + randArr[2]);
+}
+
+
+
+//shuffle hint array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 // Allow the "Player" to Play Again
@@ -182,9 +176,18 @@ $("#Hint").click(provideHint);
 $("#PlayAgain").click(playAgain);
 
 $(document).keypress(function(e) {
-    if(e.which == 13) {
-        playersGuessSubmission();
-    }
+  if(e.which == 13) {
+    playersGuessSubmission();
+  }
+  $("Message").empty();
+});
+
+//disable hint after it was clicked once
+$().ready(function() {
+  $("#Hint").click(function () {
+    $(this).hide();
+    return true;
+  });
 });
 
 }
